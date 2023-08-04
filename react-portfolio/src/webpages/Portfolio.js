c252a6c8be3647e3542643fa4ca305a7f2e9fc08
Aegean <todddharni@gmail.com>
2023-08-04 09:13:41 -0600

    Added routing and page samples

---
+++ react-portfolio/src/webpages/Portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+// Page for my Portfolios Structure/Content
+function Portfolio() {
+  return (
+    <div>
+      <Header />
+      <h3>Portfolio</h3>
+      <Footer />
+    </div>
+  )
+}
+
+export default Portfolio
