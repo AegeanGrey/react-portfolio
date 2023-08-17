eb2d3d4910d771f3b5e0716df7e9f1866b24ef84
Aegean <todddharni@gmail.com>
2023-08-17 01:02:35 -0600

    Added structure/base for contact form

--- react-portfolio/src/webpages/Contact.js
+++ react-portfolio/src/webpages/Contact.js
@@ -7,7 +7,20 @@ function Contact() {
   return (
     <div>
       <Header />
-      <h3 class='Portfolio-Title'>Contact</h3>
+      <h3 className='contactTitle'>Contact</h3>
+
+    <div className='alignForm'>
+      <form>
+        <h2>Name:</h2>
+        <input placeholder='John Doe'/>
+        <h2>Email Address:</h2>
+        <input placeholder='username@service.com'/>
+        <h2>Message:</h2>
+        <textarea rows='6' placeholder='How can I help you?'/>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+
       <Footer />
     </div>
   )
